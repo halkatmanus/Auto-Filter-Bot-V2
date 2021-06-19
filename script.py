6baf9bd90598ff35e02bab4eeb2d1c213b8af946
Siddharth Raut <66844790+halkatmanus@users.noreply.github.com>
2021-06-19 17:44:04 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,13 +3,11 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
+🖐🏻Hey there! I am Maushi.
 
-This is Version 2 of Auto Filter Bot
+I can get videos for you. But you have to join my group @RequestVideos
 
 Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
 
 For more click <i>help</i></b>"""
 
@@ -18,10 +16,8 @@ For more click <i>help</i></b>"""
 
 <i>
 * Add bot to your group with admin rights.
-
 * Add bot to channels which you want to link with <b>all admin rights</b>!
 </i>
-
 
 <b>Bot Commands - Works in Group only</b>
 (You need to be a Auth User in order to use these commands)
@@ -32,38 +28,30 @@ or
 
 <i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
 
-
 * <code>/del channelid</code>  -  Delinks channel from group
 or
 * <code>/del @channelusername</code>  -  Delinks channel from group
 
 <i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
 
-
 * <code>/delall</code>  -  Removes all connected channels and filters from group!
 
 <i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
 
-
 * <code>/filterstats</code>  -  Check connected channels and number of filters.
 
+No need add each filter again! Bot will automatically search for your files and give links to that!
+
+<b>@RequestVideos</b>"""
 
 
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : Maushi</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @HalkatManus
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
 
 """
