3582b8bf3c448ccfc66e29c01641d523b6fdedbd
Siddharth Raut <66844790+halkatmanus@users.noreply.github.com>
2021-06-23 12:47:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -9,36 +9,36 @@ Bot gives button link to files if available !
 </b>"""
 
 
-    HELP_MSG = """<b>How to use the bot??</b>
-
-<i>
+    HELP_MSG = """<b>How to use the bot??</b> <i>
 * Add bot to your group with admin rights.
 * Add bot to channels which you want to link with <b>all admin rights</b>!
 </i>
+
 <b>Bot Commands - Works in Group only</b>
 (You need to be a Auth User in order to use these commands)
 
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
+Link channel to your group.
+* <code>/add</code> channelid
+OR
+* <code>/add</code> @channelusername
 <i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
 
-* <code>/del channelid</code>  -  Delinks channel from group
+Delinks channel from group
+* <code>/del</code> channelid
 or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
+* <code>/del</code> @channelusername
 <i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
 
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
+Removes all connected channels and filters from group!
+* <code>/delall</code>
 <i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
 
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
+Check connected channels and number of filters.
+* <code>/filterstats</code>
 
-No need add each filter again! Bot will automatically search for your files and give links to that!
+No need add each filters again! Bot will automatically search for your files and give links to that!
 
-<b>@RequestVideos</b>"""
+<b>@VlDE0S | @RequestVideos</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Maushi</b>
